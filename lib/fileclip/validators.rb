1588d6f11feaf0177b4029cd44f7a774f28e1ba9
Nate Clark <nate@hired.com>
2014-01-03 21:54:27 -0800

    update validators to avoid db:setup race condition

--- lib/fileclip/validators.rb
+++ lib/fileclip/validators.rb
@@ -4,7 +4,7 @@ module FileClip
     module HelperMethods
       %w{validates_attachment_content_type validates_attachment_presence validates_attachment_size}.each do |method|        
         define_method(method) do |*attr_names|
-          if self.column_names.include? "#{attr_names.first}_filepicker_url"
+          if method_defined?("#{attr_names.first}_filepicker_url".to_sym)
             attr_names.last.merge!({ :if => "#{attr_names.first}_filepicker_url_not_present?".to_sym })
           end
           super(*attr_names)
